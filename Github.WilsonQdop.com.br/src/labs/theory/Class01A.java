48071a905399529627857b7175a0e7253463f951
WilsonQdop <wilson.qdop@gmail.com>
2025-10-15 17:11:43 -0300

    docs(Aula 1 á 2): Aprendendo a teoria do que é a programação orientada a objetos e qual é a definição de um objeto

---
+++ Github.WilsonQdop.com.br/src/labs/theory/Class01A.java
@@ -0,0 +1,21 @@
+package labs.theory;
+
+// O que é a programação orientada a objetos
+public class Class01A {
+
+    /*
+        Alan Kay foi o primeiro "programador" quem pensou na programação orientado a objetos.
+        Criou uma das primeiras linguagens orientada a objetos que é o SmallTalk.
+        Esse paradigma facilita a transferência de dados entre os próprios objetos.
+
+        Vantagens -->
+                    (COMERN)ada = Comer nada
+
+                     c = Confiabilidade (Ao alterar uma parte, nenhuma outra é afetada)
+                     o = Oportuno (Ao dividir tudo em partes, várias delas podem ser desenvolvidas em paralelo)
+                     m = Manutenível (Manutenção).
+                     e = Extensível (Possível a expansão de funcionalidades)
+                     r = Reutilizável
+                     n = Natural (Se preocupar mais na funcionalidade do que nos detalhes de implementação)
+    */
+}
---
+++ Github.WilsonQdop.com.br/src/labs/theory/Class02A.java
@@ -0,0 +1,31 @@
+package labs.theory;
+
+// O que é um objeto?
+public class Class02A {
+    /*
+        Um objeto é uma coisa material ou abstrata que pode ser percebido pelos -
+        - sentidos e descritos pelas suas características e estado atual.
+
+        Nos conceitos voltando a Orientação a Objetos, uma classe para existir precisa responder três perguntas -->
+                        ( Seguindo como base um objeto que seja uma (CANETA) )
+            "Que coisas eu tenho" (Atributos/Características)
+                Modelo,
+                Cor,
+                Ponta,
+                Carga,
+                Tampa
+            "Coisas que eu faço" (Métodos/Comportamento)
+                Escrever,
+                Rabiscar,
+                Pintar,
+                Tampar,
+                Destampar
+            "Como eu estou agora" (estado)
+                Modelo = "Bic",
+                Cor = "Azul",
+                Tampa = True
+
+        O Objeto em si existe através de uma classe. Onde essa classe é um molde para algo que existe
+        na realidade e o objeto é instanciado dessa classe para se criar
+    */
+}
